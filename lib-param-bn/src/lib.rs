00ce9f9c323b0ec0e2a7253f2d5e38224edbfec2
Samuel Pastva <sam.pastva@gmail.com>
2021-12-22 23:11:14 +0100

    Bindings for the Boolean network.

--- lib-param-bn/src/lib.rs
+++ lib-param-bn/src/lib.rs
@@ -1,7 +1,8 @@
 extern crate biodivine_bdd;
 extern crate biodivine_lib_param_bn;
 
-use biodivine_lib_param_bn::Monotonicity;
+use biodivine_bdd::BooleanExpression;
+use biodivine_lib_param_bn::{FnUpdate, Monotonicity};
 use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
@@ -238,11 +239,233 @@ impl RegulatoryGraph {
     pub fn variables(&self) -> Vec<VariableId> {
         self.0.variables().map(|it| it.into()).collect()
     }
+
+    /// Get a list of all regulations.
+    pub fn regulations(&self, py: Python) -> PyResult<Vec<PyObject>> {
+        let mut result = Vec::new();
+        for reg in self.0.regulations() {
+            let dict = PyDict::new(py);
+            dict.set_item("source", VariableId(reg.get_regulator()).into_py(py))?;
+            dict.set_item("target", VariableId(reg.get_target()).into_py(py))?;
+            if let Some(m) = reg.get_monotonicity() {
+                let m = match m {
+                    Monotonicity::Activation => "activation",
+                    Monotonicity::Inhibition => "inhibition",
+                };
+                dict.set_item("monotonicity", m)?;
+            }
+            dict.set_item("observable", reg.is_observable())?;
+            result.push(dict.to_object(py));
+        }
+        Ok(result)
+    }
+}
+
+#[pyclass]
+#[derive(Clone, Copy)]
+pub struct ParameterId(biodivine_lib_param_bn::ParameterId);
+
+impl From<ParameterId> for biodivine_lib_param_bn::ParameterId {
+    fn from(value: ParameterId) -> Self {
+        value.0
+    }
+}
+
+impl From<biodivine_lib_param_bn::ParameterId> for ParameterId {
+    fn from(value: biodivine_lib_param_bn::ParameterId) -> Self {
+        ParameterId(value)
+    }
+}
+
+#[pymethods]
+impl ParameterId {
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.0))
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.0))
+    }
 }
 
 #[pyclass]
 #[derive(Clone)]
 pub struct BooleanNetwork(biodivine_lib_param_bn::BooleanNetwork);
+
+impl From<BooleanNetwork> for biodivine_lib_param_bn::BooleanNetwork {
+    fn from(value: BooleanNetwork) -> Self {
+        value.0
+    }
+}
+
+impl From<biodivine_lib_param_bn::BooleanNetwork> for BooleanNetwork {
+    fn from(value: biodivine_lib_param_bn::BooleanNetwork) -> Self {
+        BooleanNetwork(value)
+    }
+}
+
+#[pymethods]
+impl BooleanNetwork {
+    /// Create a new Boolean network with no functions using a regulatory graph.
+    #[new]
+    pub fn new(graph: RegulatoryGraph) -> BooleanNetwork {
+        BooleanNetwork(biodivine_lib_param_bn::BooleanNetwork::new(graph.into()))
+    }
+
+    /// Create a Boolean network from an `.sbml` string.
+    #[staticmethod]
+    pub fn from_sbml(model: String) -> PyResult<BooleanNetwork> {
+        let model = biodivine_lib_param_bn::BooleanNetwork::try_from_sbml(model.as_str());
+        match model {
+            Ok(model) => Ok(model.0.into()),
+            Err(e) => Err(PyTypeError::new_err(e)),
+        }
+    }
+
+    /// Create a Boolean network from a `.bnet` string.
+    #[staticmethod]
+    pub fn from_bnet(model: String) -> PyResult<BooleanNetwork> {
+        let model = biodivine_lib_param_bn::BooleanNetwork::try_from_bnet(model.as_str());
+        match model {
+            Ok(model) => Ok(model.into()),
+            Err(e) => Err(PyTypeError::new_err(e)),
+        }
+    }
+
+    /// Create a Boolean network from an `.aeon` string.
+    #[staticmethod]
+    pub fn from_aeon(model: String) -> PyResult<BooleanNetwork> {
+        let model: Result<biodivine_lib_param_bn::BooleanNetwork, String> =
+            std::convert::TryFrom::try_from(model.as_str());
+        match model {
+            Ok(model) => Ok(model.into()),
+            Err(e) => Err(PyTypeError::new_err(e)),
+        }
+    }
+
+    /// Write this network into an `.sbml` string.
+    pub fn to_sbml(&self) -> String {
+        self.0.to_sbml(None)
+    }
+
+    /// Write this network into a `.bnet` string.
+    pub fn to_bnet(&self) -> PyResult<String> {
+        match self.0.to_bnet() {
+            Ok(data) => Ok(data),
+            Err(error) => Err(PyTypeError::new_err(error)),
+        }
+    }
+
+    /// Write this network into an `.aeon` string.
+    pub fn to_aeon(&self) -> String {
+        self.0.to_string()
+    }
+
+    /// Get the underlying regulatory graph of this Boolean network.
+    pub fn graph(&self) -> RegulatoryGraph {
+        self.0.as_graph().clone().into()
+    }
+
+    /// Set an update function for the given variable.
+    ///
+    /// Use `None` to remove update function, otherwise use a string that will be automatically
+    /// parsed. Note that the network must contain all used variables and parameters.
+    pub fn set_update_function(
+        &mut self,
+        variable: &PyAny,
+        function: Option<String>,
+    ) -> PyResult<()> {
+        let graph = self.graph();
+        let variable = self.graph().find_variable(variable)?;
+        let result = if let Some(function) = function {
+            let expression = BooleanExpression::parse(function.as_str())?;
+            let function = FnUpdate::try_from_expression(expression.into(), &graph.into());
+            if let Some(function) = function {
+                self.0.set_update_function(variable.into(), Some(function))
+            } else {
+                Err("Function expression uses unknown variables.".to_string())
+            }
+        } else {
+            self.0.set_update_function(variable.into(), None)
+        };
+
+        match result {
+            Ok(()) => Ok(()),
+            Err(e) => Err(PyTypeError::new_err(e)),
+        }
+    }
+
+    /// Create a new parameter from a dictionary containing the parameter arity and name.
+    pub fn add_parameter(&mut self, parameter: &PyDict) -> PyResult<ParameterId> {
+        let name = parameter.get_item("name");
+        let name = if let Some(name) = name {
+            name.extract::<String>()?
+        } else {
+            return Err(PyTypeError::new_err("Expected string name."));
+        };
+
+        let arity = parameter.get_item("arity");
+        let arity = if let Some(arity) = arity {
+            arity.extract::<u32>()?
+        } else {
+            return Err(PyTypeError::new_err("Expected integer arity."));
+        };
+
+        let result = self.0.add_parameter(name.as_str(), arity);
+        match result {
+            Ok(id) => Ok(id.into()),
+            Err(e) => Err(PyTypeError::new_err(e)),
+        }
+    }
+
+    /// Get the number of variables in this network.
+    pub fn num_vars(&self) -> usize {
+        self.0.num_vars()
+    }
+
+    /// Get the number of parameters in this network.
+    pub fn num_parameters(&self) -> usize {
+        self.0.num_parameters()
+    }
+
+    /// Get a list of variable ids in this network.
+    pub fn variables(&self) -> Vec<VariableId> {
+        self.0.variables().into_iter().map(|it| it.into()).collect()
+    }
+
+    /// Get a list of parameter ids in this network.
+    pub fn parameters(&self) -> Vec<ParameterId> {
+        self.0
+            .parameters()
+            .into_iter()
+            .map(|it| it.into())
+            .collect()
+    }
+
+    /// Get a string expression representing the update function of the given variable.
+    pub fn get_update_function(&self, variable: &PyAny) -> PyResult<Option<String>> {
+        let graph = self.graph();
+        let variable = graph.find_variable(variable)?;
+        let function = self.0.get_update_function(variable.into());
+        Ok(function.as_ref().map(|fun| fun.to_string(&self.0)))
+    }
+
+    /// Find a `ParameterId` for a parameter of the given name.
+    pub fn find_parameter(&self, parameter: &PyAny) -> PyResult<ParameterId> {
+        if let Ok(name) = parameter.extract::<String>() {
+            let id = self.0.find_parameter(name.as_str());
+            if let Some(id) = id {
+                Ok(id.into())
+            } else {
+                Err(PyTypeError::new_err(format!("Parameter {} unknown.", name)))
+            }
+        } else if let Ok(id) = parameter.extract::<ParameterId>() {
+            Ok(id)
+        } else {
+            Err(PyTypeError::new_err(format!("Expected parameter name.")))
+        }
+    }
+}
 
 #[pyclass]
 #[derive(Clone)]
