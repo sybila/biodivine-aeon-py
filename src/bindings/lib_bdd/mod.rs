58a82a6cdc773f44b421a4529f84f3a73faef987
Samuel Pastva <daemontus@gmail.com>
2022-03-22 19:28:11 +0100

    Properly register BddVariableSetBuilder.

--- src/bindings/lib_bdd/mod.rs
+++ src/bindings/lib_bdd/mod.rs
@@ -14,6 +14,7 @@ pub(crate) fn register(module: &PyModule) -> PyResult<()> {
     module.add_class::<PyBddVariable>()?;
     module.add_class::<PyBddVariableSet>()?;
     module.add_class::<PyBooleanExpression>()?;
+    module.add_class::<PyBddVariableSetBuilder>()?;
     Ok(())
 }
 
