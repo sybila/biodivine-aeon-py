10f5e46d2be970b2ff37dd90c78fb758d92daff3
Samuel Pastva <sam.pastva@gmail.com>
2022-12-06 23:09:52 +0100

    Add `FixedPoints` class, only with symbolic algorithms for now.

---
+++ src/bindings/lib_param_bn/_impl_fixed_points.rs
@@ -0,0 +1,114 @@
+use crate::bindings::lib_param_bn::{
+    PyFixedPoints, PyGraphColoredVertices, PyGraphColors, PyGraphVertices, PySymbolicAsyncGraph,
+};
+use crate::AsNative;
+use biodivine_lib_param_bn::fixed_points::FixedPoints;
+use pyo3::pymethods;
+
+#[pymethods]
+impl PyFixedPoints {
+    /// A naive symbolic algorithm that computes the fixed points by gradual
+    /// elimination of all states with outgoing transitions.
+    #[staticmethod]
+    #[args(restriction = "None")]
+    pub fn naive_symbolic(
+        stg: &PySymbolicAsyncGraph,
+        restriction: Option<&PyGraphColoredVertices>,
+    ) -> PyGraphColoredVertices {
+        if let Some(restriction) = restriction {
+            FixedPoints::naive_symbolic(stg.as_native(), restriction.as_native()).into()
+        } else {
+            let restriction = stg.as_native().unit_colored_vertices();
+            FixedPoints::naive_symbolic(stg.as_native(), restriction).into()
+        }
+    }
+
+    /// A better version of the `Self::naive_symbolic` algorithm that can actually scale to
+    /// reasonably sized networks (e.g. 100-200 variables + parameters).
+    ///
+    // Only fixed-points from the restriction set are returned. However, the state has to be a
+    // global fixed point, not just a fixed-point within the restriction set.
+    #[staticmethod]
+    #[args(restriction = "None")]
+    pub fn symbolic(
+        stg: &PySymbolicAsyncGraph,
+        restriction: Option<&PyGraphColoredVertices>,
+    ) -> PyGraphColoredVertices {
+        if let Some(restriction) = restriction {
+            FixedPoints::symbolic(stg.as_native(), restriction.as_native()).into()
+        } else {
+            let restriction = stg.as_native().unit_colored_vertices();
+            FixedPoints::symbolic(stg.as_native(), restriction).into()
+        }
+    }
+
+    /// The result of the function are all vertices that can appear as fixed-points for some
+    /// parameter valuation. That is, for every returned vertex, there is at least one color
+    /// for which the vertex is a fixed-point.
+    #[staticmethod]
+    #[args(restriction = "None")]
+    pub fn symbolic_vertices(
+        stg: &PySymbolicAsyncGraph,
+        restriction: Option<&PyGraphColoredVertices>,
+    ) -> PyGraphVertices {
+        if let Some(restriction) = restriction {
+            FixedPoints::symbolic_vertices(stg.as_native(), restriction.as_native()).into()
+        } else {
+            let restriction = stg.as_native().unit_colored_vertices();
+            FixedPoints::symbolic_vertices(stg.as_native(), restriction).into()
+        }
+    }
+
+    /// Similar to `Self::symbolic_vertices`, but only returns colors for which there exists
+    /// at least one fixed-point within restriction.
+    #[staticmethod]
+    #[args(restriction = "None")]
+    pub fn symbolic_colors(
+        stg: &PySymbolicAsyncGraph,
+        restriction: Option<&PyGraphColoredVertices>,
+    ) -> PyGraphColors {
+        if let Some(restriction) = restriction {
+            FixedPoints::symbolic_colors(stg.as_native(), restriction.as_native()).into()
+        } else {
+            let restriction = stg.as_native().unit_colored_vertices();
+            FixedPoints::symbolic_colors(stg.as_native(), restriction).into()
+        }
+    }
+
+    /// This function creates an iterator that yields symbolic sets of fixed-point states, such
+    /// that eventually, all fixed-points are returned (and there are no duplicates).
+    ///
+    /// As with `Self::symbolic`, you can use the restriction set to limit the search to
+    /// a particular subset of states. Furthermore, you can use size_limit to give the algorithm
+    /// a hint as to how large (in terms of BDD nodes) you want the resulting sets to be.
+    /// This value depends on how much memory and time you have, but a good starting value
+    /// tends to be somewhere between 1_000_000 and 10_000_000. If you'd rather want more smaller
+    /// sets, but get them quickly, you can go as low as 10_000 or 100_000 (given the model
+    /// is not too large).
+    ///
+    /// It is not strictly guaranteed that the results are within this size limit, but the
+    /// algorithm makes a reasonable effort to achieve this.
+    ///
+    /// WARNING: Due to technical difficulties, right now it is not possible to actually use
+    /// this as an iterator, and instead you can use `item_limit` to set a limit on the number
+    /// of returned items.
+    #[staticmethod]
+    #[args(restriction = "None")]
+    pub fn symbolic_list(
+        stg: &PySymbolicAsyncGraph,
+        size_limit: usize,
+        item_limit: Option<usize>,
+        restriction: Option<&PyGraphColoredVertices>,
+    ) -> Vec<PyGraphColoredVertices> {
+        let item_limit = item_limit.unwrap_or(usize::MAX);
+        let it = if let Some(restriction) = restriction {
+            FixedPoints::symbolic_iterator(stg.as_native(), restriction.as_native(), size_limit)
+        } else {
+            let restriction = stg.as_native().unit_colored_vertices();
+            FixedPoints::symbolic_iterator(stg.as_native(), restriction, size_limit)
+        };
+        it.take(item_limit)
+            .map(|x| PyGraphColoredVertices::from(x))
+            .collect()
+    }
+}
--- src/bindings/lib_param_bn/mod.rs
+++ src/bindings/lib_param_bn/mod.rs
@@ -5,6 +5,7 @@ use biodivine_lib_param_bn::{BooleanNetwork, ParameterId, RegulatoryGraph, Varia
 use pyo3::prelude::*;
 
 mod _impl_boolean_network;
+mod _impl_fixed_points;
 mod _impl_graph_colored_vertices;
 mod _impl_graph_colors;
 mod _impl_graph_vertices;
@@ -22,6 +23,7 @@ pub(crate) fn register(module: &PyModule) -> PyResult<()> {
     module.add_class::<PyGraphVertices>()?;
     module.add_class::<PyGraphColoredVertices>()?;
     module.add_class::<PySymbolicAsyncGraph>()?;
+    module.add_class::<PyFixedPoints>()?;
     Ok(())
 }
 
@@ -65,3 +67,13 @@ pub struct PyGraphColoredVertices(GraphColoredVertices);
 #[pyclass(name = "SymbolicAsyncGraph")]
 #[derive(Clone)]
 pub struct PySymbolicAsyncGraph(SymbolicAsyncGraph);
+
+/// A collection of algorithms for detecting network fixed-points.
+///
+/// TODO:
+///     Right now, we can't expose the iterator API because it uses lifetimes, which pyO3
+///     can't use. Try to make a version that can be copied/arc-ed in the future?
+///     However, a bigger issue is that we can't use Z3 either :/ We must figure out
+///     how to do that...
+#[pyclass(name = "FixedPoints")]
+pub struct PyFixedPoints();
