db381750dd3b964b224c15e1732029afdddadf13
Samuel Pastva <sam.pastva@gmail.com>
2022-12-07 17:24:26 +0100

    workflow-12

--- conda/build.sh
+++ conda/build.sh
@@ -4,6 +4,8 @@ set -ex
 
 if [ `uname` == Darwin ]; then
   #  --universal2 (disabled for now)
+  export MACOSX_DEPLOYMENT_TARGET="11.0"
+  export CMAKE_OSX_DEPLOYMENT_TARGET="11.0"
   maturin build --release --interpreter python -o dist --features static-z3
 fi
 
