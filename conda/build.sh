dac981031a9071b60acc99f5ac75961440cd8dfa
Samuel Pastva <sam.pastva@gmail.com>
2023-06-05 17:36:58 +0200

    Build with the new universal binary setting in maturin.

--- conda/build.sh
+++ conda/build.sh
@@ -5,8 +5,7 @@ set -ex
 if [ `uname` == Darwin ]; then  
   export MACOSX_DEPLOYMENT_TARGET="11.0"
   export CMAKE_OSX_DEPLOYMENT_TARGET="11.0"
-  #  --universal2 (disabled for now)
-  maturin build --release --interpreter python -o dist --features static-z3
+  maturin build --target universal2-apple-darwin --release --interpreter python -o dist --features static-z3
 fi
 
 if [ `uname` == Linux ]; then
