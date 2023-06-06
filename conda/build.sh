c8adb0638964dc3cead00213468449f6722fdc19
Samuel Pastva <sam.pastva@gmail.com>
2023-06-06 21:17:04 +0200

    Revert to conda without universal binaries.

--- conda/build.sh
+++ conda/build.sh
@@ -5,8 +5,10 @@ set -ex
 if [ `uname` == Darwin ]; then  
   export MACOSX_DEPLOYMENT_TARGET="11.0"
   export CMAKE_OSX_DEPLOYMENT_TARGET="11.0"
-  rustup target add aarch64-apple-darwin
-  maturin build --target universal2-apple-darwin --release --interpreter python -o dist --features static-z3
+  # For whatever reason, I cannot seem to successfully install the arm target such that maturin can use it...
+  # rustup target add aarch64-apple-darwin
+  # --target universal2-apple-darwin
+  maturin build --release --interpreter python -o dist --features static-z3
 fi
 
 if [ `uname` == Linux ]; then
