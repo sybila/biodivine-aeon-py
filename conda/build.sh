65035ef629edf53c6a02682639bde6bd3ba333db
Samuel Pastva <sam.pastva@gmail.com>
2023-06-06 20:55:06 +0200

    Fix conda build for universal binaries.

--- conda/build.sh
+++ conda/build.sh
@@ -5,6 +5,7 @@ set -ex
 if [ `uname` == Darwin ]; then  
   export MACOSX_DEPLOYMENT_TARGET="11.0"
   export CMAKE_OSX_DEPLOYMENT_TARGET="11.0"
+  rustup target add aarch64-apple-darwin
   maturin build --target universal2-apple-darwin --release --interpreter python -o dist --features static-z3
 fi
 
