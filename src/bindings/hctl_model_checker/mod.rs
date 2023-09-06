76fa78d244838e05749cae78427ebba7b27b6349
Samuel Pastva <sam.pastva@gmail.com>
2023-09-06 22:20:11 +0200

    Use a single polymorphic function instead of two variants.

--- src/bindings/hctl_model_checker/mod.rs
+++ src/bindings/hctl_model_checker/mod.rs
@@ -3,16 +3,17 @@ use crate::bindings::lib_param_bn::{
 };
 use biodivine_hctl_model_checker::analysis::{analyse_formula, analyse_formulae};
 use biodivine_hctl_model_checker::mc_utils::get_extended_symbolic_graph;
-use biodivine_hctl_model_checker::model_checking::{
-    model_check_formula, model_check_multiple_formulae, model_check_tree, model_check_trees,
-};
+use biodivine_hctl_model_checker::model_checking::{model_check_tree, model_check_trees};
 use biodivine_hctl_model_checker::preprocessing::node::HctlTreeNode;
+use biodivine_hctl_model_checker::preprocessing::parser::parse_and_minimize_hctl_formula;
 use biodivine_hctl_model_checker::result_print::PrintOptions;
+use biodivine_lib_param_bn::BooleanNetwork;
 
-use crate::{throw_runtime_error, AsNative};
+use crate::{throw_runtime_error, throw_type_error, AsNative};
 
 use macros::Wrapper;
 use pyo3::prelude::*;
+use pyo3::types::PyList;
 use pyo3::PyResult;
 
 mod _impl_hctl_tree_node;
@@ -23,8 +24,6 @@ pub(crate) fn register(module: &PyModule) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(get_extended_stg, module)?)?;
     module.add_function(wrap_pyfunction!(model_check, module)?)?;
     module.add_function(wrap_pyfunction!(model_check_multiple, module)?)?;
-    module.add_function(wrap_pyfunction!(model_check_hctl_tree, module)?)?;
-    module.add_function(wrap_pyfunction!(model_check_multiple_hctl_trees, module)?)?;
     module.add_function(wrap_pyfunction!(mc_analysis, module)?)?;
     module.add_function(wrap_pyfunction!(mc_analysis_multiple, module)?)?;
     Ok(())
@@ -47,55 +46,58 @@ pub fn get_extended_stg(
     }
 }
 
+impl PyHctlTreeNode {
+    /// Try to read a HCTL tree node from a dynamic Python type. This can be either:
+    ///
+    ///  - `PyHctlTreeNode` itself;
+    ///  - A string that will be parsed as a HCTL formula.
+    pub(crate) fn from_python(any: &PyAny, network: &BooleanNetwork) -> PyResult<PyHctlTreeNode> {
+        if let Ok(val) = any.extract::<PyHctlTreeNode>() {
+            Ok(val)
+        } else if let Ok(string) = any.extract::<String>() {
+            let parsed = parse_and_minimize_hctl_formula(network, string.as_str());
+            match parsed {
+                Err(e) => throw_runtime_error(e),
+                Ok(tree) => Ok(PyHctlTreeNode::from(tree)),
+            }
+        } else {
+            throw_type_error("Expected a HCTL formula or a HCTL tree node.")
+        }
+    }
+}
+
 #[pyfunction]
-/// Run the model checking algorithm on a HCTL formula.
+/// Run the model checking algorithm on a HCTL formula ([String] or [PyHctlTreeNode]).
+///
 /// Returns a satisfying color-state relation.
 pub fn model_check(
-    formula: String,
+    formula: &PyAny,
     stg: &PySymbolicAsyncGraph,
 ) -> PyResult<PyGraphColoredVertices> {
-    match model_check_formula(formula, stg.as_native()) {
+    let stg = stg.as_native();
+    let formula = PyHctlTreeNode::from_python(formula, stg.as_network())?;
+    match model_check_tree(formula.into(), stg) {
         Ok(result) => Ok(result.into()),
         Err(error) => throw_runtime_error(error),
     }
 }
 
 #[pyfunction]
-/// Run the model checking algorithm on a list of HCTL formulae.
+/// Run the model checking algorithm on a list of HCTL formulae (each formula can be a [String]
+/// or a [HctlTreeNode]).
+///
 /// Returns a list of satisfying color-state relations, one for each formula.
 pub fn model_check_multiple(
-    formulae: Vec<String>,
+    formulae: &PyList,
     stg: &PySymbolicAsyncGraph,
 ) -> PyResult<Vec<PyGraphColoredVertices>> {
-    match model_check_multiple_formulae(formulae, stg.as_native()) {
-        Ok(results) => Ok(results.iter().map(|r| r.clone().into()).collect()),
-        Err(error) => throw_runtime_error(error),
+    let stg = stg.as_native();
+    let mut list: Vec<HctlTreeNode> = Vec::new();
+    for formula in formulae {
+        list.push(PyHctlTreeNode::from_python(formula, stg.as_network())?.into());
     }
-}
-
-#[pyfunction]
-/// Run the model checking algorithm on a HCTL formula given by its syntactic tree.
-/// Returns a satisfying color-state relation.
-pub fn model_check_hctl_tree(
-    tree: PyHctlTreeNode,
-    stg: &PySymbolicAsyncGraph,
-) -> PyResult<PyGraphColoredVertices> {
-    match model_check_tree(tree.as_native().clone(), stg.as_native()) {
-        Ok(result) => Ok(result.into()),
-        Err(error) => throw_runtime_error(error),
-    }
-}
-
-#[pyfunction]
-/// Run the model checking algorithm on a list of HCTL formulae given by their syntactic trees.
-/// Returns a list of satisfying color-state relations, one for each formula.
-pub fn model_check_multiple_hctl_trees(
-    trees: Vec<PyHctlTreeNode>,
-    stg: &PySymbolicAsyncGraph,
-) -> PyResult<Vec<PyGraphColoredVertices>> {
-    let native_trees = trees.iter().map(|r| r.as_native().clone()).collect();
-    match model_check_trees(native_trees, stg.as_native()) {
-        Ok(results) => Ok(results.iter().map(|r| r.clone().into()).collect()),
+    match model_check_trees(list, stg) {
+        Ok(results) => Ok(results.into_iter().map(|r| r.into()).collect()),
         Err(error) => throw_runtime_error(error),
     }
 }
