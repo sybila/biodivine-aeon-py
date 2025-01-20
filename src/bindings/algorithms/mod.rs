1dac29a3949d80515a39a665ea9aed1d3978914f
Ondřej Hložek <hlozek.o1@gmail.com>
2025-01-20 15:27:43 +0100

    Fix typo in files.

--- src/bindings/algorithms/mod.rs
+++ src/bindings/algorithms/mod.rs
@@ -2,11 +2,11 @@ use pyo3::{
     types::{PyModule, PyModuleMethods},
     Bound, PyResult,
 };
-use reachibility_config::ReachabilityConfig;
+use reachability_config::ReachabilityConfig;
 
-// mod reachibility;
-mod reachibility_config;
-mod reachibility_error;
+// mod reachability;
+mod reachability_config;
+mod reachability_error;
 
 pub fn register(module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_class::<ReachabilityConfig>()?;
