14649cbd7faeb8a588642c63c3a084a0207b78fc
Ondřej Hložek <hlozek.o1@gmail.com>
2025-03-15 18:37:21 +0100

    Added support for logging.

--- src/bindings/algorithms/mod.rs
+++ src/bindings/algorithms/mod.rs
@@ -4,6 +4,7 @@ pub mod cancellation;
 pub mod reachability;
 
 pub fn register(module: &Bound<'_, PyModule>) -> PyResult<()> {
+    pyo3_log::init();
     reachability::register(module)?;
     Ok(())
 }
