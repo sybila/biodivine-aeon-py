eea50a6c1515646a9c9533881b1012c3447c3ae9
Ondřej Hložek <hlozek.o1@gmail.com>
2025-03-22 19:18:54 +0100

    Added prepare to merge helper function.

--- src/bindings/algorithms/fixed_points/fixed_points_impl.rs
+++ src/bindings/algorithms/fixed_points/fixed_points_impl.rs
@@ -1,3 +1,4 @@
+use biodivine_lib_bdd::Bdd;
 use biodivine_lib_param_bn::{
     biodivine_std::traits::Set, symbolic_async_graph::GraphColoredVertices,
 };
@@ -9,6 +10,7 @@ use crate::bindings::algorithms::fixed_points::{
 };
 
 const TARGET_NAIVE_SYMBOLIC: &str = "FixedPoints::naive_symbolic";
+const TARGET_SYMBOLIC: &str = "FixedPoints::symbolic";
 
 #[pyclass(module = "biodivine_aeon", frozen)]
 pub struct FixedPoints(FixedPointsConfig);
@@ -18,13 +20,16 @@ impl FixedPoints {
     pub fn config(&self) -> &FixedPointsConfig {
         &self.0
     }
+}
 
+impl FixedPoints {
     // TODO: ohtenkay - document this, discuss whether to take the documentation from lib_param_bn
     pub fn naive_symbolic(
         &self,
         restriction: &GraphColoredVertices,
     ) -> Result<GraphColoredVertices, FixedPointsError> {
-        info!(target: TARGET_NAIVE_SYMBOLIC,
+        info!(
+            target: TARGET_NAIVE_SYMBOLIC,
             "Started search with {}[nodes:{}] candidates.",
             restriction.approx_cardinality(),
             restriction.symbolic_size()
@@ -37,11 +42,16 @@ impl FixedPoints {
             .map(|var| {
                 let can_step = stg.var_can_post(var, stg.unit_colored_vertices());
                 let is_stable = restriction.minus(&can_step);
-                trace!(target: TARGET_NAIVE_SYMBOLIC,
+
+                // interrupt()?;
+
+                trace!(
+                    target: TARGET_NAIVE_SYMBOLIC,
                     " > Created initial set for {:?} using {} BDD nodes.",
                     var,
                     is_stable.symbolic_size()
                 );
+
                 is_stable
             })
             .collect();
@@ -51,10 +61,13 @@ impl FixedPoints {
 
             // TODO: ohtenkay - ask what should be debug and what slould be trace
             debug!(
+                target: TARGET_NAIVE_SYMBOLIC,
                 " > Merging {} sets using {} BDD nodes.",
                 to_merge.len(),
                 to_merge.iter().map(|it| it.symbolic_size()).sum::<usize>(),
             );
+
+            // interrupt()?;
 
             let x = to_merge.pop().unwrap();
             let y = to_merge.pop().unwrap();
@@ -63,7 +76,8 @@ impl FixedPoints {
 
         // TODO: ohtenkay - discuss this error
         let Some(fixed_points) = to_merge.pop() else {
-            info!(target: TARGET_NAIVE_SYMBOLIC,
+            info!(
+                target: TARGET_NAIVE_SYMBOLIC,
                 "No fixed points found using {} BDD nodes.",
                 restriction.symbolic_size()
             );
@@ -71,11 +85,63 @@ impl FixedPoints {
         };
 
         info!(
+            target: TARGET_NAIVE_SYMBOLIC,
             "Found {}[nodes:{}] fixed-points.",
             fixed_points.approx_cardinality(),
             fixed_points.symbolic_size(),
         );
 
         Ok(fixed_points)
+    }
+
+    pub fn symbolic(
+        &self,
+        restriction: &GraphColoredVertices,
+    ) -> Result<GraphColoredVertices, FixedPointsError> {
+        info!(
+            target: TARGET_SYMBOLIC,
+            "Started search with {}[nodes:{}] candidates.",
+            restriction.approx_cardinality(),
+            restriction.symbolic_size()
+        );
+
+        let stg = &self.config().graph;
+        let mut to_merge = self.prepare_to_merge(TARGET_SYMBOLIC)?;
+
+        // Finally add the global requirement on the whole state space, if it is relevant.
+        if !stg.unit_colored_vertices().is_subset(restriction) {
+            to_merge.push(restriction.as_bdd().clone());
+        }
+
+        // interrupt()?;
+
+        Ok(restriction.clone())
+    }
+}
+
+impl FixedPoints {
+    fn prepare_to_merge(&self, target: &str) -> Result<Vec<Bdd>, FixedPointsError> {
+        let stg = &self.config().graph;
+
+        let result = stg
+            .variables()
+            .map(|var| {
+                let can_step = stg.var_can_post(var, stg.unit_colored_vertices());
+                let is_stable = stg.unit_colored_vertices().minus(&can_step);
+
+                // interrupt()?;
+
+                trace!(
+                    target: target,
+                    " > Created initial set for {:?} using {} BDD nodes.",
+                    var,
+                    is_stable.symbolic_size()
+                );
+
+                is_stable.into_bdd()
+            })
+            .collect();
+
+        Ok(result)
     }
 }
