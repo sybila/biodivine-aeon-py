772fd2251187c9f9216b5df05f495237ce045d19
Ondřej Hložek <hlozek.o1@gmail.com>
2025-03-23 20:02:07 +0100

    Added cancellation and creation from Python from FixedPointsConfig

--- src/bindings/algorithms/fixed_points/fixed_points_config.rs
+++ src/bindings/algorithms/fixed_points/fixed_points_config.rs
@@ -1,11 +1,28 @@
+use std::time::Duration;
+
 use biodivine_lib_param_bn::symbolic_async_graph::{GraphColoredVertices, SymbolicAsyncGraph};
-use pyo3::pyclass;
+use pyo3::{pyclass, pymethods, Py};
+
+use crate::{
+    bindings::{
+        algorithms::cancellation::{
+            tokens::{CancelTokenPython, CancelTokenTimer},
+            CancellationHandler,
+        },
+        lib_param_bn::symbolic::{
+            asynchronous_graph::AsynchronousGraph, set_colored_vertex::ColoredVertexSet,
+        },
+    },
+    AsNative,
+};
 
 #[pyclass(module = "biodivine_aeon", frozen)]
+#[derive(Clone)]
 pub struct FixedPointsConfig {
     pub graph: SymbolicAsyncGraph,
     pub restriction: GraphColoredVertices,
-    // TODO: ohtenkay - discuss the rest of the parameters.
+    pub cancellation: Box<dyn CancellationHandler>,
+    // TODO: ohtenkay - discuss the rest of the fields.
 }
 
 impl FixedPointsConfig {
@@ -13,6 +30,7 @@ impl FixedPointsConfig {
     pub fn with_graph(graph: SymbolicAsyncGraph) -> Self {
         FixedPointsConfig {
             restriction: graph.unit_colored_vertices().clone(),
+            cancellation: Default::default(),
             graph,
         }
     }
@@ -21,5 +39,63 @@ impl FixedPointsConfig {
     pub fn with_restriction(mut self, restriction: GraphColoredVertices) -> Self {
         self.restriction = restriction;
         self
+    }
+
+    /// Update the `cancellation` property, automatically wrapping the [CancellationHandler]
+    /// in a `Box`.
+    pub fn with_cancellation<C: CancellationHandler + 'static>(mut self, cancellation: C) -> Self {
+        self.cancellation = Box::new(cancellation);
+        self
+    }
+}
+
+impl CancellationHandler for FixedPointsConfig {
+    fn is_cancelled(&self) -> bool {
+        self.cancellation.is_cancelled()
+    }
+
+    fn start_timer(&self) {
+        self.cancellation.start_timer()
+    }
+}
+
+#[pymethods]
+impl FixedPointsConfig {
+    #[new]
+    #[pyo3(signature = (graph, restriction = None, time_limit_millis = None))]
+    pub fn new_py(
+        graph: Py<AsynchronousGraph>,
+        restriction: Option<Py<ColoredVertexSet>>,
+        time_limit_millis: Option<u64>,
+    ) -> Self {
+        let mut config = FixedPointsConfig::with_graph(graph.get().as_native().clone());
+
+        if let Some(restriction) = restriction {
+            config = config.with_restriction(restriction.get().as_native().clone())
+        }
+
+        if let Some(millis) = time_limit_millis {
+            config = config.with_cancellation(CancelTokenPython::with_inner(CancelTokenTimer::new(
+                Duration::from_millis(millis),
+            )))
+        }
+
+        config
+    }
+
+    #[staticmethod]
+    #[pyo3(name = "with_graph")]
+    pub fn with_graph_py(graph: Py<AsynchronousGraph>) -> Self {
+        FixedPointsConfig::with_graph(graph.get().as_native().clone())
+            .with_cancellation(CancelTokenPython::default())
+    }
+
+    // TODO: if we ever move away from abi3-py37, use Duration as an argument
+    #[pyo3(name = "with_time_limit")]
+    pub fn with_time_limit_py(&self, duration_in_millis: u64) -> Self {
+        self.clone()
+            .with_cancellation(CancelTokenPython::with_inner(CancelTokenTimer::new(
+                Duration::from_millis(duration_in_millis),
+            )))
     }
 }
