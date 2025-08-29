a911625820973b4a5b01a8e8463a3e0b60efb483
Ondřej Hložek <hlozek.o1@gmail.com>
2025-08-30 00:41:23 +0200

    Fix typo in filename.

--- src/bindings/algorithms/reachability/mod.rs
+++ src/bindings/algorithms/reachability/mod.rs
@@ -1,13 +1,13 @@
 use pyo3::{
-    types::{PyModule, PyModuleMethods as _},
     Bound, PyResult,
+    types::{PyModule, PyModuleMethods as _},
 };
 
 use crate::internal::algorithms::reachability::{Reachability, ReachabilityConfig};
 
 mod _impl_pyerr;
 mod reachability_config_python;
-mod reachablility_impl_python;
+mod reachability_impl_python;
 
 pub fn register(module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_class::<Reachability>()?;
